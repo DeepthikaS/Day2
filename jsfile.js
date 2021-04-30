9692a72c7385d92cb8abd8073db4788a2c120a27
DeepthikaS <83287008+DeepthikaS@users.noreply.github.com>
2021-04-30 11:23:41 +0530

    30/4/2021

--- jsfile.js
+++ jsfile.js
@@ -5,5 +5,8 @@ function display(val){
 function calculate(){
     var a=document.getElementById("dis").value;
     var b=eval(a);
+    if(b==undefined){
+        b=null;
+    }
     document.getElementById("dis").value=b;
 }
